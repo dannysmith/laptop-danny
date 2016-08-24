4d14b895ac806598f9a8aa0fce267df2486b0d89
Danny Smith <hi@danny.is>
2016-08-24 17:09:13 +0100

    Added browser-sync

--- node.sh
+++ node.sh
@@ -8,6 +8,6 @@ npm update -g
 
 fancy_echo "Istalling global NPM packages..."
 
-npm install -g gitbook-cli grunt-cli pryjs nodemon
+npm install -g gitbook-cli grunt-cli pryjs nodemon browser-sync
 
 green_echo "Done with Node/NPM installs!"
